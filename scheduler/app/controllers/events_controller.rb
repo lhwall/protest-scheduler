885efaada1a28425740be681964366654bf56359
Lucy Wall <lhwall@gmail.com>
2018-06-28 15:28:15 +0000

    new event and user set up in controllers

---
+++ scheduler/app/controllers/events_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+get "/new_event" do
+  if logged_in
+  erb :new_event
+else
+  redirect to "/log_in"
+end
+
+post "/new_event" do
+  if logged_in
+    @event = Event.create(params)
+    erb :event_detail
+  else
+    direct to "/log_in"
+  end
+end
+
+end
