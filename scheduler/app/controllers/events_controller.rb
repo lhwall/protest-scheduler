2456e7885b41000651aa0795c639da9fa2a0ab9d
Lucy Wall <lhwall@gmail.com>
2018-06-28 19:07:19 +0000

    add can create a new event type or select an existing type when creating an event

--- scheduler/app/controllers/events_controller.rb
+++ scheduler/app/controllers/events_controller.rb
@@ -11,7 +11,16 @@ end
 
 post "/new_event" do
   if logged_in
-    @event = Event.create(params)
+    @event = Event.create(:name => params[:name], :location => params[:location], :date => params[:date], :time => params[:time], :description => params[:description])
+    if params[:new_category] != ""
+       @category = Category.new(:name => params[:new_category])
+       @category.save
+       @event.category_id = @category.id
+    else
+      binding.pry
+      @event.category_id = params[:category]
+      @event.save
+      end
     erb :"events/event_detail"
   else
     direct to "/log_in"
