705fd19e74d4213975d125dffeb64522e920d465
Lucy Wall <lhwall@gmail.com>
2018-06-29 02:19:45 +0000

    set up update method

--- scheduler/app/controllers/events_controller.rb
+++ scheduler/app/controllers/events_controller.rb
@@ -28,16 +28,15 @@ end
 
 get "/events/:id" do
 @event = Event.find(params[:id])
-erb :"/events/event_detail"
+erb :"events/event_detail"
 end
 
 get "/events/:id/update" do
-#binding.pry
 if logged_in
-  #binding.pry
   @event = Event.find(params[:id])
   if @event.user == current_user
-    erb :"/events/update_event"
+    binding.pry
+    erb :"events/update_event"
   else
     redirect to "/"
 end
@@ -48,6 +47,19 @@ end
 patch "/events/:id/update" do
   #binding.pry
   @event = Event.find(params[:id])
+  if @event.user == current_user
+    @event = Event.update(:name => params[:name], :location => params[:location], :date => params[:date], :time => params[:time], :description => params[:description], :user_id => current_user.id)
+    # if params[:new_category] != ""
+    #    @category = Category.new(:name => params[:new_category])
+    #    @category.save
+    #    @event.category_id = @category.id
+    # else
+    #   @event.category_id = params[:category]
+      @event.save
+    erb :"events/event_detail"
+  else
+    redirect to "/user_index"
+  end
 end
 
 delete "/events/:id/delete" do
